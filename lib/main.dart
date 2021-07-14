2f93afd4ee34798fc641c9f4ea6d71e6e891cce0
mizuhomai <choco.hana.choco@gmail.com>
2021-07-14 16:55:41 +0900

    バックグラウンドブルー タイトル作成

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anyting'),
+            backgroundColor: Colors.indigoAccent,
+          ),
+        ),
+      ),
+  );
+}
+
+
